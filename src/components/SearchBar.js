8f929d84fef7225afc07c67615f440bd6cfe88db
Iman Chamileva <iman.moussaevna@gmail.com>
2022-03-14 16:45:18 +0100

    commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {useState} from "react"
 
 
 
@@ -7,21 +7,42 @@ import React from 'react'
 function SearchBar({placeholder, data}) {
 
 
+    const [filteredData, setFilteredData] = useState ([])
+
+    const handleFilter = (e) => {
+        const searchWord = e.target.value
+        const newFilter= data.filter((value) => {
+            return value.position.toLowerCase().includes(searchWord.toLowerCase())
+        })
+
+        if (searchWord === "") {
+            setFilteredData([])
+        } else {
+            setFilteredData(newFilter)
+        }
+        
+    }
+
+
+
     return(
 
         <div className="divGenerale">
 
             <div className="jobsearch">
-                <input placeholder={placeholder} ></input>
+                <input placeholder={placeholder} onChange={handleFilter}/>
+                
+            {filteredData.length != 0 && (
                <div className="dataResult">
-                   {data.map((value,key) => {
+                   {filteredData.slice(0,15).map((value,key) => {
                        return (<a href className="dataItem" >
-                          <p> {value.company}</p>
+                          <p> {value.position}</p>
                            </a>
                        )
                    })}
-               </div>
-            </div>
+               </div>)
+}
+</div>      
 
             <div className="location">
                 <input placeholder="Location"></input>
